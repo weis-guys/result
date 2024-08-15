44918edbd222f05b48c99b81a768e489bdb271b3
JacobWeisenburger <jacob.weisenburger@gmail.com>
2024-08-15 11:43:33 -0500

    1.0.2

--- src/Result.ts
+++ src/Result.ts
@@ -8,7 +8,7 @@ export module Result {
         value: Value
     }
 
-    export type Warning<WarningType = undefined> = {
+    export type Warning<WarningType> = {
         type: 'warning'
         success: true
         /**
@@ -17,7 +17,7 @@ export module Result {
         warning: WarningType
     }
 
-    export type ValueAndWarning<Value = undefined, WarningType = undefined> = {
+    export type ValueAndWarning<Value, WarningType> = {
         type: 'valueAndWarning'
         success: true
         /**
@@ -30,16 +30,21 @@ export module Result {
         warning: WarningType
     }
 
-    export type Error<Error> = {
+    export type Error<ErrorType = string> = {
         type: 'error'
         success: false
         /**
          * a fatal error
          */
-        error: Error
+        error: ErrorType
     }
 
-    export type Any<ValueType = any, ErrorType = any, WarningType = any> =
+    // Result.Ok<string> | Result.Error<string>
+    export type Either<ValueType, ErrorType = string> =
+        | Ok<ValueType>
+        | Error<ErrorType>
+
+    export type Any<ValueType = unknown, ErrorType = string, WarningType = unknown> =
         | Ok<ValueType>
         | Warning<WarningType>
         | ValueAndWarning<ValueType, WarningType>
@@ -55,17 +60,16 @@ export module Result {
     /**
      * return a successful result with a warning
      */
-    export function warn<const WarningType = undefined> ( warning: WarningType ): Warning<WarningType> {
+    export function warn<const WarningType> ( warning: WarningType ): Warning<WarningType> {
         return { type: 'warning', success: true, warning }
     }
 
     /**
      * return a successful result with value and a warning
      */
-    export function valueAndWarning<
-        const Value = undefined,
-        const Warning = undefined,
-    > ( value: Value, warning: Warning ): ValueAndWarning<Value, Warning> {
+    export function valueAndWarning<const Value, const Warning> (
+        value: Value, warning: Warning
+    ): ValueAndWarning<Value, Warning> {
         return { type: 'valueAndWarning', success: true, value, warning }
     }
 
@@ -84,6 +88,6 @@ export module Result {
      * return only the value of a perticualr property if it exists
      */
     export const get = ( key: PropKeys ) => <Result extends Any> ( result: Result ) => {
-        return ( result as any )[ key ]
+        return ( result as Record<string, any> )[ key ]
     }
 }
